6820cbe65d737b815c15a7624f4c209c53805c55
vladnen97 <darkdescent1991@gmail.com>
2023-10-02 22:40:06 +0300

    feat: new stories with controlled modals

--- src/components/ui/modal/modal.stories.tsx
+++ src/components/ui/modal/modal.stories.tsx
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
+
+import type { Meta } from '@storybook/react'
 
 import Button from '../button/button'
 
 import { Modal } from './'
 
-import { EditOutline, TrashOutline } from 'assets/icons'
 import { Checkbox } from 'components/ui/checkbox'
 import { TextField } from 'components/ui/text-field'
-import { Typography } from 'components/ui/typography'
 
 const meta = {
   title: 'Components/UI/Modal',
@@ -21,49 +21,30 @@ const meta = {
 } satisfies Meta<typeof Modal>
 
 export default meta
-type Story = StoryObj<typeof meta>
 
-export const AddNewPack: Story = {
-  args: {
-    trigger: <Button>Add New Pack</Button>,
-    title: 'Add New Pack',
-    children: (
+const ModalWithHooks = (props: { title?: string }) => {
+  const [open, setOpen] = useState<boolean>(false)
+
+  return (
+    <Modal
+      open={open}
+      trigger={<Button>Add New Pack</Button>}
+      onClose={() => setOpen(state => !state)}
+      title={props.title}
+    >
       <div style={{ display: 'flex', gap: '24px', flexDirection: 'column' }}>
         <TextField label="Name Pask" />
         <Checkbox label="Private pack" />
         <Button>Add Pack</Button>
       </div>
-    ),
-  },
+    </Modal>
+  )
 }
 
-export const EditPack: Story = {
-  args: {
-    trigger: <EditOutline />,
-    title: 'Edit Pack',
-    children: (
-      <div style={{ display: 'flex', gap: '24px', flexDirection: 'column' }}>
-        <TextField label="Name Pask" />
-        <Checkbox label="Private pack" />
-        <Button>Save Changes</Button>
-      </div>
-    ),
-  },
+export const Default = {
+  render: () => <ModalWithHooks title={'Add New Pack'} />,
 }
 
-export const DeletePack: Story = {
-  args: {
-    trigger: <TrashOutline />,
-    title: 'Delete Pack',
-    children: (
-      <div style={{ display: 'flex', gap: '24px', flexDirection: 'column' }}>
-        <Typography style={{ whiteSpace: 'pre-line' }}>
-          Do you really want to remove Pack Name?
-          {'\n'}
-          All cards will be deleted.
-        </Typography>
-        <Button>Delete Pack</Button>
-      </div>
-    ),
-  },
+export const WithoutTitle = {
+  render: () => <ModalWithHooks />,
 }
