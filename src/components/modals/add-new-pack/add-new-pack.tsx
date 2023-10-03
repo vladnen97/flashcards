61c91b8b66191d41ea9912868bc69f90e936a8d3
vladnen97 <darkdescent1991@gmail.com>
2023-10-03 23:29:55 +0300

    feat: modal with form for add new deck

---
+++ src/components/modals/add-new-pack/add-new-pack.tsx
@@ -0,0 +1,36 @@
+import s from './add-new-pack.module.scss'
+import { FormValues, useAddNewPack } from './use-add-new-pack.ts'
+
+import Button from 'components/ui/button/button.tsx'
+import { ControlledCheckbox, ControlledTextField } from 'components/ui/controlled'
+import { Modal } from 'components/ui/modal'
+
+type Props = {
+  open: boolean
+  onClose: () => void
+  onSubmit: (data: FormValues) => void
+}
+
+export const AddNewPack = (props: Props) => {
+  const { handleSubmit, control } = useAddNewPack()
+
+  return (
+    <Modal
+      trigger={<Button>Add New Deck</Button>}
+      title={'Add New Deck'}
+      open={props.open}
+      onClose={props.onClose}
+    >
+      <form onSubmit={handleSubmit(props.onSubmit)} className={s.content}>
+        <ControlledTextField name={'deckName'} control={control} label={'Name Deck'} />
+        <ControlledCheckbox name={'isPrivate'} control={control} label={'Private deck'} />
+        <div className={s.btnGroup}>
+          <Button onClick={props.onClose} type={'button'} variant={'secondary'}>
+            Cancel
+          </Button>
+          <Button type={'submit'}>Add New Deck</Button>
+        </div>
+      </form>
+    </Modal>
+  )
+}
