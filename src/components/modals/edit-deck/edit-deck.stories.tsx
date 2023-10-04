083f19e145a1b6a9b7d4a8cb724fda577494e2fb
vladnen97 <darkdescent1991@gmail.com>
2023-10-04 22:08:11 +0300

    feat: make trigger come throw props

--- src/components/modals/edit-deck/edit-deck.stories.tsx
+++ src/components/modals/edit-deck/edit-deck.stories.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 
 import type { Meta } from '@storybook/react'
 
+import { EditOutline } from 'assets/icons'
 import { FormValues } from 'components/modals/add-new-pack'
 import { EditDeck } from 'components/modals/edit-deck'
+import Button from 'components/ui/button/button.tsx'
 
 const meta = {
   title: 'Components/MODALS/EditPack',
@@ -18,7 +20,18 @@ const EditDeckWithHooks = () => {
 
   const handleSubmit = (data: FormValues) => console.log(data)
 
-  return <EditDeck open={open} onClose={() => setOpen(state => !state)} onSubmit={handleSubmit} />
+  return (
+    <EditDeck
+      open={open}
+      onClose={() => setOpen(state => !state)}
+      onSubmit={handleSubmit}
+      trigger={
+        <Button>
+          <EditOutline /> Edit
+        </Button>
+      }
+    />
+  )
 }
 
 export const Default = {
