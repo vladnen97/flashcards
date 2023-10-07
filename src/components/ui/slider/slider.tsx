28d485ae113ce29cdcaeeee3314278aa81c192e0
vladnen97 <darkdescent1991@gmail.com>
2023-10-07 20:07:30 +0300

    fix: add label;

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -2,14 +2,19 @@ import { FC } from 'react'
 
 import * as SliderRadix from '@radix-ui/react-slider'
 
+import { Typography } from 'components/ui/typography'
+
+// eslint-disable-next-line
 import s from './slider.module.scss'
 
 type SliderProps = SliderRadix.SliderProps & {
   multiple?: boolean
   value: number[]
+  label?: string
 }
 
 export const Slider: FC<SliderProps> = ({
+  label,
   value,
   onValueChange,
   onValueCommit,
@@ -20,47 +25,52 @@ export const Slider: FC<SliderProps> = ({
   step = 1,
 }) => {
   return (
-    <div className={disabled ? s.disabled : s.container}>
-      {value.length !== 1 && (
+    <div>
+      <Typography variant="body2" className={s.label}>
+        {label || ''}
+      </Typography>
+      <div className={disabled ? s.disabled : s.container}>
+        {value.length !== 1 && (
+          <input
+            type="number"
+            value={value[0]}
+            onChange={e => {
+              if (Number(e.target.value) > max) return
+              onValueChange!([Number(e.target.value), value[1]])
+              e.target.value = String(Number(e.target.value))
+            }}
+            className={s.value}
+          />
+        )}
+        <SliderRadix.Root
+          className={s.root}
+          value={value}
+          onValueChange={newValue => {
+            onValueChange!(newValue)
+          }}
+          onValueCommit={onValueCommit}
+          disabled={disabled}
+          min={min}
+          max={max}
+          step={step}
+        >
+          <SliderRadix.Track className={s.track}>
+            <SliderRadix.Range className={s.range} />
+          </SliderRadix.Track>
+          <SliderRadix.Thumb className={s.thumb} />
+          {multiple && <SliderRadix.Thumb className={s.thumb} />}
+        </SliderRadix.Root>
         <input
           type="number"
-          value={value[0]}
+          value={value.length === 1 ? value[0] : value[1]}
           onChange={e => {
             if (Number(e.target.value) > max) return
-            onValueChange!([Number(e.target.value), value[1]])
+            onValueChange!([value[0], Number(e.target.value)])
             e.target.value = String(Number(e.target.value))
           }}
           className={s.value}
         />
-      )}
-      <SliderRadix.Root
-        className={s.root}
-        value={value}
-        onValueChange={newValue => {
-          onValueChange!(newValue)
-        }}
-        onValueCommit={onValueCommit}
-        disabled={disabled}
-        min={min}
-        max={max}
-        step={step}
-      >
-        <SliderRadix.Track className={s.track}>
-          <SliderRadix.Range className={s.range} />
-        </SliderRadix.Track>
-        <SliderRadix.Thumb className={s.thumb} />
-        {multiple && <SliderRadix.Thumb className={s.thumb} />}
-      </SliderRadix.Root>
-      <input
-        type="number"
-        value={value.length === 1 ? value[0] : value[1]}
-        onChange={e => {
-          if (Number(e.target.value) > max) return
-          onValueChange!([value[0], Number(e.target.value)])
-          e.target.value = String(Number(e.target.value))
-        }}
-        className={s.value}
-      />
+      </div>
     </div>
   )
 }
