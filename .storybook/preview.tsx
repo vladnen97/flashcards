9c2e681482523b85abb6df4c13f67cb4fb55df79
vladnen97 <darkdescent1991@gmail.com>
2023-10-08 21:59:28 +0300

    feat: wrap story in provider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,6 +2,8 @@ import type { Preview } from '@storybook/react'
 import '../src/styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import { Provider } from 'react-redux'
+import { store } from '../src/services/store'
 
 const preview: Preview = {
   parameters: {
@@ -13,6 +15,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    Story => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 }
 
 export default preview
