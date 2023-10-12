82ef57a75890e44f7505e4ba1fe275852b753929
vladnen97 <darkdescent1991@gmail.com>
2023-10-12 10:14:29 +0300

    feat: move states to hook

--- src/pages/decks/add-new-pack/add-new-pack.tsx
+++ src/pages/decks/add-new-pack/add-new-pack.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 import s from './add-new-pack.module.scss'
-import { FormValues, useAddNewPack } from './use-add-new-pack.ts'
+import { useAddNewPack } from './use-add-new-pack.ts'
 
 import Button from 'components/ui/button/button.tsx'
 import { ControlledCheckbox, ControlledTextField } from 'components/ui/controlled'
@@ -9,21 +9,23 @@ import { Modal } from 'components/ui/modal'
 
 type Props = {
   trigger: ReactNode
-  open: boolean
-  onClose: () => void
-  onSubmit: (data: FormValues) => void
 }
 
 export const AddNewPack = (props: Props) => {
-  const { handleSubmit, control } = useAddNewPack()
+  const { open, setOpen, handleSubmit, control, handleCreateDeckSubmit } = useAddNewPack()
 
   return (
-    <Modal trigger={props.trigger} title={'Add New Deck'} open={props.open} onClose={props.onClose}>
-      <form onSubmit={handleSubmit(props.onSubmit)} className={s.content}>
+    <Modal
+      trigger={props.trigger}
+      title={'Add New Deck'}
+      open={open}
+      onClose={() => setOpen(state => !state)}
+    >
+      <form onSubmit={handleSubmit(handleCreateDeckSubmit)} className={s.content}>
         <ControlledTextField name={'name'} control={control} label={'Name Deck'} />
         <ControlledCheckbox name={'isPrivate'} control={control} label={'Private deck'} />
         <div className={s.btnGroup}>
-          <Button onClick={props.onClose} type={'button'} variant={'secondary'}>
+          <Button onClick={() => setOpen(false)} type={'button'} variant={'secondary'}>
             Cancel
           </Button>
           <Button type={'submit'}>Add New Deck</Button>
