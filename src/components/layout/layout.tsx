fb2f97b2bf84831bc65dd27d6feced4c3a20ef9a
vladnen97 <darkdescent1991@gmail.com>
2023-10-17 20:01:41 +0300

    feat: auth data transfer to header

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,12 +2,25 @@ import { Outlet } from 'react-router-dom'
 
 import s from './layout.module.scss'
 
+import { useLogoutMutation, useMeQuery } from '@/services/auth/auth-api.ts'
 import { Header } from 'components/ui/header'
 
 export const Layout = () => {
+  const { data } = useMeQuery()
+  const [logout] = useLogoutMutation()
+
+  let userData = null
+
+  if (data) {
+    userData = { name: '', email: '', avatar: null as string | null }
+    userData.name = data.name
+    userData.email = data.email
+    userData.avatar = data.avatar
+  }
+
   return (
     <>
-      <Header userData={null} onSignOut={() => {}} />
+      <Header userData={userData} onSignOut={logout} />
       <main className={s.container}>
         <Outlet />
       </main>
