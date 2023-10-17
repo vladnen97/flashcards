fb7f37bb435fd26ac388dfa86411d00678b3cde4
vladnen97 <darkdescent1991@gmail.com>
2023-10-17 20:01:21 +0300

    feat: add signin path use Me querry for auth

--- src/router.tsx
+++ src/router.tsx
@@ -9,12 +9,18 @@ import {
 import { Cards } from '@/pages/cards'
 import { Decks } from '@/pages/decks'
 import { Learn } from '@/pages/learn'
+import { SignInPage } from '@/pages/sign-in'
+import { useMeQuery } from '@/services/auth/auth-api.ts'
 import { Layout } from 'components/layout'
 
 const publicRoutes: RouteObject[] = [
   {
-    path: '/login',
-    element: <div>login</div>,
+    path: '/sign-in',
+    element: <SignInPage />,
+  },
+  {
+    path: '/sign-up',
+    element: <div>Sign up</div>,
   },
 ]
 
@@ -51,7 +57,11 @@ export const Router = () => {
 }
 
 function PrivateRoutes() {
-  const isAuthenticated = true
+  const { data, isLoading } = useMeQuery()
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+  if (isLoading) return <div>Loading...</div>
+
+  const isAuthenticated = !!data
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/sign-in" />
 }
