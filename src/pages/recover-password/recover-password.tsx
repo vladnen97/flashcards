4e0d46f57b054387948e752fad603ebfbbb26cd1
vladnen97 <darkdescent1991@gmail.com>
2023-10-18 23:57:37 +0300

    feat: recover pass page

---
+++ src/pages/recover-password/recover-password.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import { useRecoverPasswordMutation } from '@/services/auth'
+import { CheckEmail } from 'components/auth/check-email'
+import { RecoverPassword } from 'components/auth/recover-password'
+
+export const RecoverPasswordPage = () => {
+  const [show, setShow] = useState<string>('')
+  const [recoverPassword] = useRecoverPasswordMutation()
+  const handleRecoverPass = (data: { email: string }) => {
+    const html: string =
+      '<h1>Hi, ##name##</h1><p>Click <a href="http://localhost:5173/reset-password/##token##">here</a> to recover your password</p>'
+
+    recoverPassword({ html, ...data })
+      .unwrap()
+      .then(() => {
+        setShow(data.email)
+      })
+  }
+
+  return show ? <CheckEmail email={show} /> : <RecoverPassword onSubmit={handleRecoverPass} />
+}
