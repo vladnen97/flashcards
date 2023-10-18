378e04d7aad64fb06dc7cdd4413b53bfc11bcd89
vladnen97 <darkdescent1991@gmail.com>
2023-10-18 23:59:30 +0300

    feat: new validation string - name

--- src/components/auth/sign-up/sign-up.schema.ts
+++ src/components/auth/sign-up/sign-up.schema.ts
@@ -2,6 +2,11 @@ import { z } from 'zod'
 
 export const signUpSchema = z
   .object({
+    name: z
+      .string()
+      .nonempty('Enter your name')
+      .min(3, 'Name must contain at least 3 character(s)')
+      .max(10, 'Name must contain less then 10 character(s)'),
     email: z.string().nonempty('Enter email').email('Invalid email address'),
     password: z.string().min(3, 'Password must contain at least 3 character(s)'),
     confirmPassword: z.string().min(3, 'Password must contain at least 3 character(s)'),
