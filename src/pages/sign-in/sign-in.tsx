d21ba5b6c57da24da88018c8f3655262f10a0be2
vladnen97 <darkdescent1991@gmail.com>
2023-10-18 23:59:56 +0300

    feat: handle sign in

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,8 +1,23 @@
-import { useLoginMutation } from '@/services/auth/auth-api.ts'
-import { SignIn } from 'components/auth/sign-in'
+import { Navigate, useNavigate } from 'react-router-dom'
+
+import { useLoginMutation, useMeQuery } from '@/services/auth/auth-api.ts'
+import { FormValues, SignIn } from 'components/auth/sign-in'
 
 export const SignInPage = () => {
-  const [signIn] = useLoginMutation()
+  const { data, isLoading } = useMeQuery()
+  const [signIn, { isLoading: isSigningIn }] = useLoginMutation()
+  const navigate = useNavigate()
 
-  return <SignIn onSubmit={signIn} />
+  if (isLoading) return <div>Loading...</div>
+  if (data) return <Navigate to={'/'} />
+
+  const handleSignIn = (data: FormValues) => {
+    signIn(data)
+      .unwrap()
+      .then(() => {
+        navigate('/')
+      })
+  }
+
+  return <SignIn onSubmit={handleSignIn} isSubmitting={isSigningIn} />
 }
