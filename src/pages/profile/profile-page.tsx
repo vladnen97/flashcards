f1c952e650041b2c079d07141981dfe6a683f33a
vladnen97 <darkdescent1991@gmail.com>
2023-10-19 12:40:35 +0300

    fix: refactoring

--- src/pages/profile/profile-page.tsx
+++ src/pages/profile/profile-page.tsx
@@ -1,3 +1,34 @@
+import { useState } from 'react'
+
+import { useLogoutMutation, useMeQuery, useUpdateProfileMutation } from '@/services/auth'
+import { Profile } from 'components/auth/profile'
+import { ProfileSchemaType } from 'components/auth/profile/use-profile-form.ts'
+
 export const ProfilePage = () => {
-  return <div>Profile</div>
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+  const { data, isLoading } = useMeQuery()
+  const [logout] = useLogoutMutation()
+  const [updateProfile] = useUpdateProfileMutation()
+
+  const handleUpdateProfile = (data: ProfileSchemaType) => {
+    updateProfile({ name: data.nickName })
+      .unwrap()
+      .then(() => {
+        setIsEdit(false)
+      })
+  }
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <Profile
+      isEdit={isEdit}
+      setIsEdit={setIsEdit}
+      name={data?.name || ''}
+      email={data?.email || ''}
+      src={data?.avatar}
+      onSubmit={handleUpdateProfile}
+      onSignOut={logout}
+    />
+  )
 }
