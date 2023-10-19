91e1077f3d2d6cb3296f806df638143ff344c688
vladnen97 <darkdescent1991@gmail.com>
2023-10-19 12:40:03 +0300

    feat: make profile page

--- src/components/auth/profile/profile.tsx
+++ src/components/auth/profile/profile.tsx
@@ -1,23 +1,21 @@
-import { ChangeEvent, FC } from 'react'
+import { FC } from 'react'
 
 import s from './profile.module.scss'
 
 import { EditOutline, LogOutOutline } from 'assets/icons'
 import { ProfileSchemaType, useProfileForm } from 'components/auth/profile/use-profile-form.ts'
-import { Avatar } from 'components/ui/avatar/avatar.tsx'
 import Button from 'components/ui/button/button.tsx'
 import { Card } from 'components/ui/card'
 import { ControlledTextField } from 'components/ui/controlled'
 import { Typography } from 'components/ui/typography'
 
 type ProfileProps = {
+  isEdit: boolean
+  setIsEdit: (value: boolean) => void
   name: string
   email: string
-  src: string
-  handleChangeAvatar: (event: ChangeEvent<HTMLInputElement>) => void
-  handleLogout: () => void
-  showTextField: boolean
-  handleChangeName: () => void
+  src?: string | null
+  onSignOut?: () => void
   onSubmit: (data: ProfileSchemaType) => void
 }
 
@@ -25,49 +23,43 @@ export const Profile: FC<ProfileProps> = ({
   name,
   email,
   src,
-  handleChangeAvatar,
-  handleLogout,
-  handleChangeName,
-  showTextField,
+  isEdit,
+  setIsEdit,
+  onSignOut,
   onSubmit,
 }) => {
-  const { handleSubmit, control } = useProfileForm(onSubmit)
+  const { handleSubmit, control } = useProfileForm({ name })
 
   return (
     <Card className={s.card}>
       <Typography variant="large" className={s.title}>
         Personal Information
       </Typography>
-      <div className={s.imgBlock}>
-        <Avatar src={src} name="user" size={96} />
-        <label>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleChangeAvatar}
-            style={{ display: 'none' }}
-          />
-          <Button variant="secondary" as={'span'} className={s.img}>
-            <EditOutline />
-          </Button>
-        </label>
-      </div>
-      {!showTextField ? (
+      {/*<div className={s.imgBlock}>*/}
+      {/*  <Avatar src={src} name="user" size={96} />*/}
+      {/*  <label>*/}
+      {/*    <input type="file" accept="image/*" style={{ display: 'none' }} />*/}
+      {/*    <Button variant="secondary" as={'span'} className={s.img}>*/}
+      {/*      <EditOutline />*/}
+      {/*    </Button>*/}
+      {/*  </label>*/}
+      {/*</div>*/}
+      {!isEdit ? (
         <>
           <Typography variant="h1" className={s.name}>
-            {name} <EditOutline onClick={handleChangeName} />
+            {name} <EditOutline onClick={() => setIsEdit(true)} />
           </Typography>
           <Typography variant="body2" className={s.email}>
             {email}
           </Typography>
-          <Button variant="secondary" onClick={handleLogout} className={s.logoutBtn}>
+          <Button variant="secondary" onClick={onSignOut} className={s.logoutBtn}>
             <LogOutOutline /> <Typography>Logout</Typography>
           </Button>
         </>
       ) : (
-        <form onSubmit={handleSubmit} className={s.form}>
-          <ControlledTextField label="Nickname" name="nickName" control={control} defaultValue="" />
-          <Button className={s.submitBtn} fullWidth>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+          <ControlledTextField label="Nickname" name="nickName" control={control} />
+          <Button className={s.submitBtn} fullWidth type={'submit'}>
             Save Changes
           </Button>
         </form>
