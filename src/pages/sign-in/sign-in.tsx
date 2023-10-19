fece90a87d382fa6bd00d7cc2d63e0a745566762
vladnen97 <darkdescent1991@gmail.com>
2023-10-19 23:21:07 +0300

    feat: remove redirect after success response

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 import { useLoginMutation, useMeQuery } from '@/services/auth/auth-api.ts'
 import { FormValues, SignIn } from 'components/auth/sign-in'
@@ -6,17 +6,12 @@ import { FormValues, SignIn } from 'components/auth/sign-in'
 export const SignInPage = () => {
   const { data, isLoading } = useMeQuery()
   const [signIn, { isLoading: isSigningIn }] = useLoginMutation()
-  const navigate = useNavigate()
 
   if (isLoading) return <div>Loading...</div>
   if (data) return <Navigate to={'/'} />
 
   const handleSignIn = (data: FormValues) => {
     signIn(data)
-      .unwrap()
-      .then(() => {
-        navigate('/')
-      })
   }
 
   return <SignIn onSubmit={handleSignIn} isSubmitting={isSigningIn} />
