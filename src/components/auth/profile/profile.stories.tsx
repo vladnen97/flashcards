f092bb018251e143ef3116de8b31391ee9352bc2
vladnen97 <darkdescent1991@gmail.com>
2023-10-21 23:51:09 +0300

    feat: fix stories args

--- src/components/auth/profile/profile.stories.tsx
+++ src/components/auth/profile/profile.stories.tsx
@@ -24,41 +24,16 @@ export const Default = () => {
   const img =
     'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png'
 
-  const [showTextField, setShowTextField] = useState(false)
-  const handleChangeAvatar = (event: any) => {
-    const file = event.target.files && event.target.files[0]
-
-    if (file) {
-      const reader = new FileReader()
-
-      reader.onloadend = () => {
-        console.log('reader.onloadend')
-      }
-      reader.readAsDataURL(file)
-    }
-  }
-
-  const handleLogout = () => {
-    console.log('handleLogout')
-  }
-
-  const handleChangeName = () => {
-    setShowTextField(true)
-  }
+  const [showTextField, setShowTextField] = useState<boolean>(false)
 
   return (
     <Profile
       name={'Sergey'}
       email={'sergey.ose.pyatigorsk@gmail.com'}
       src={img}
-      handleChangeAvatar={handleChangeAvatar}
-      handleLogout={handleLogout}
-      showTextField={showTextField}
-      handleChangeName={handleChangeName}
-      onSubmit={data => {
-        console.log(data)
-        setShowTextField(false)
-      }}
+      isEdit={showTextField}
+      setIsEdit={setShowTextField}
+      onSignOut={() => {}}
     />
   )
 }
